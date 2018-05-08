e51f4f2419ce0e357b283b5138056d4a81e85800
irvinktang <irvink.tang@gmail.com>
2018-05-08 17:51:26 -0400

    Set up environment

---
+++ src/client/components/Form.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+    render() {
+        return <h1>Yes I am</h1>
+    }
+}
+
+export default Form;
---
+++ src/client/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Form from './components/Form.jsx';
+
+import styles from './styles/app.css';
+
+render(
+    <Form />,
+    document.getElementById('root')
+);
