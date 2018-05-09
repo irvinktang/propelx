acf5c7474ebfbb2fd0029aeb88d244b340aaab1d
irvinktang <irvink.tang@gmail.com>
2018-05-09 01:47:58 -0400

    Add POST request to REST API

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.once('open', function() {
+    console.log('Success: connected to MongoDb!');
+});
+mongoose.connection.on('error', function(err) {
+    process.exit(1);
+});
 
 app.use(express.static(__dirname + './../../'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.post("/", function(req,res) {
+    console.log("It works!");
+})
+
 app.listen(PORT);
